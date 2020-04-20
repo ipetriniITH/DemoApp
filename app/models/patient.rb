ec77e179d40440cabd6c82df8934743d903aff02
ipetriniITH <ipetriniITH@intouchhealth.com>
2020-04-20 16:43:46 -0300

    Add some validations to patients model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,19 @@ class Patient < ApplicationRecord
   scope :sorted_by_id, lambda { order("id ASC") }
   scope :search, lambda {|query| where(["firstName LIKE ?", "%#{query}%"]) }
 
+  def fullName
+   "#{firstName} #{lastName}"
+  end
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+  validates_presence_of :firstName
+  validates_presence_of :lastName
+  validates_length_of :phone, :maximum => 10
+  validates :phone, numericality: { only_integer: true },
+                    length: { :maximum => 100}
+  validates :email, presence: true,
+                    length: { :maximum => 100},
+                    format: EMAIL_REGEX
+
 end
