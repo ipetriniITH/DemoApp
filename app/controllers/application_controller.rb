bb642e381f0924d2424df35312dd413b90124b62
ipetriniITH <ipetriniITH@intouchhealth.com>
2020-04-21 15:04:24 -0300

    Add method to confirm that user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def confirm_logged_in
+    unless session[:user_id]
+      flash[:notice] = "Please log in."
+      redirect_to(access_login_path)
+    end
+  end
 end
