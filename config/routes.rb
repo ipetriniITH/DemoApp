05e0f9ea7c823eeff51edd8f9cd539717eb15abb
ipetriniITH <ipetriniITH@intouchhealth.com>
2020-04-22 12:43:33 -0300

    Add comments to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
+#Draw routes
 Rails.application.routes.draw do
 
+  #Set root path
   root 'patients#index'
 
-  get 'admin', :to => 'access#menu'
+  #Set admin path
+  get 'admin', to: 'access#menu'
+
+  #Set paths to handle admin access
   get 'access/menu'
   get 'access/login'
   post 'access/attempt_login'
   get 'access/logout'
 
-  resources :patients do
-  
+  resources :patients do  
     member do
       get :delete
     end
-
   end
-
 end
