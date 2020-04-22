500382e72960d3df5fccf4ab0b92037210f16e47
ipetriniITH <ipetriniITH@intouchhealth.com>
2020-04-22 16:41:08 -0300

    Add comments to models methods

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,19 +4,20 @@
 class Admin < ApplicationRecord
   has_secure_password
 
-  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i.freeze
+  # Usernames that are not allowed
   FORBIDDEN_USERNAMES = %w[fstuer jlove].freeze
 
   validates :username, length: { within: 8..25 },
                        uniqueness: true
   validates :email, presence: true,
                     length: { maximum: 100 },
-                    format: EMAIL_REGEX,
+                    format: Constants::EMAIL_REGEX,
                     confirmation: true
   validate :username_is_allowed
 
   private
 
+  # Check if username is valid
   def username_is_allowed
     errors.add(:username, 'forbidden') if FORBIDDEN_USERNAMES.include?(username)
   end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,19 +2,20 @@
 
 # Define the Patient model class.
 class Patient < ApplicationRecord
-  scope :sorted_by_id, -> { order('id ASC') }
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
-  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i.freeze
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, numericality: { only_integer: true },
                     length: { maximum: 10 }
   validates :email, presence: true,
                     length: { maximum: 100 },
-                    format: EMAIL_REGEX
+                    format: Constants::EMAIL_REGEX
+
+  # Returns the patient list orderer by id
+  scope :sorted_by_id, -> { order('id ASC') }
+
+  # Returns the patient full name
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
