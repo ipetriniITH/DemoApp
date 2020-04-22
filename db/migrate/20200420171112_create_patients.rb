d11a525cc3ca6ccda5e2bc11cea17bcd28372e6b
ipetriniITH <ipetriniITH@intouchhealth.com>
2020-04-22 17:20:31 -0300

    Fix migrations styling issues

--- db/migrate/20200420171112_create_patients.rb
+++ db/migrate/20200420171112_create_patients.rb
@@ -1,10 +1,11 @@
 class CreatePatients < ActiveRecord::Migration[6.0]
   def change
     create_table :patients do |t|
-      t.string "first_name"
-      t.string "last_name"
-      t.string "email"
-      t.integer "phone"
+      t.string 'first_name'
+      t.string 'last_name'
+      t.string 'email'
+      t.integer 'phone'
+      
       t.timestamps
     end
   end
--- db/migrate/20200421154227_create_admins.rb
+++ db/migrate/20200421154227_create_admins.rb
@@ -1,8 +1,9 @@
 class CreateAdmins < ActiveRecord::Migration[6.0]
   def change
     create_table :admins do |t|
-      t.string "email", :default => '', :null => false
-      t.string "password", :limit => 40
+      t.string 'email', default: 'default@email.com', null: false
+      t.string 'password', :limit => 40
+      
       t.timestamps
     end
   end
